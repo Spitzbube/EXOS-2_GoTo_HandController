9403343f9f8872d8ac8b4e97bacd804e4fee1eb7
Spitzbube <spitzbube@mail.bg>
2019-08-30 21:46:47 +0200

    intermediate commit

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ extern void func_2254(unsigned int);
 extern void func_2328(void);
 extern void func_243c(int, int, int, char*);
 extern void func_24d4(int, int, int, char*);
+extern void func_27c4(float*, float*);
 
 extern void func_659c(int);
 extern void func_7590(void);
@@ -16,14 +17,37 @@ extern void func_5099c(void);
 extern void func_51368(void);
 
 extern char bData_40002c13; //40002c13
-float /*???*/ Data_40002e98; //40002e98
+extern char bData_40002c1a; //40002c1a
+char bData_40002e7a; //40002e7a
+float Data_40002e98; //40002e98
+float Data_40002ea8; //40002ea8
 char Data_40003196; //40003196
-
-char Data_40003592[3 /*???*/]; //40003592
+float Data_4000329c; //4000329c
+float Data_400032a0; //400032a0
+char bData_40003432; //40003432
+double Data_400034d0; //400034d0
+double Data_400034d8; //400034d8
+unsigned char Data_40003588[3 /*???*/]; //40003588
+unsigned char Data_40003592[3 /*???*/]; //40003592
+extern struct
+{
+	int fill_0[48]; //0
+	double Data_192; //192
+	double Data_200; //200
+} Data_40004128; //40004128
 char Data_40004c58[2 /*???*/]; //40004c58
 
+/* 50b40 - todo */
+void func_50b40(void)
+{
+	//TODO
+	//50c98
+	Data_400034d0 = 0.05;
+	Data_400034d8 = 0.045;
+}
+
 /* 6ab74 - todo */
-long double func_6ab74(int a)
+double func_6ab74(int a)
 {
 	char r5;
 	union
@@ -121,11 +145,6 @@ void func_6c804(void)
 /* 6d054 - todo */
 int main(void)
 {
-	//struct
-	//{
-		double d;
-	//} sp64;
-	
 	func_2328();
 	uart0_init(360);
 	uart1_init(360);
@@ -169,47 +188,37 @@ int main(void)
 	
 	func_659c(10);
 	
-	Data_40002e98 = d = func_6ab74(1);
+	Data_40002e98 = func_6ab74(1);
+
+	func_659c(50);
 	
-	d = d * 2;
-//	d = sp64.d + sp64.d;
+	Data_40002ea8 = func_6ab74(2);
+
+	func_659c(50);
+
+	if (bData_40002c1a == 1)
+	{
+		bData_40002e7a = 1;
+	}
+	else
+	{
+		bData_40002e7a = 0;
+	}
 	
-}
-
-
-double float_test(double a)
-{
-	float f1 = (int) 28827267;
-	float f2 = (unsigned int) 6376367;
-
-	double d1 = (int) 2765262;
-	double d2 = 784748;
-
-	f1 = sqrt(f1);
-	f1 = f1 * f2;
-
-	if (f1 != 0)
-	f1 = f1 / f2;
-
-    d1 = d1 + f1;
-	d1 = d1 - f1;
-	d1 = d1 + (f1 + f2);
-	d1 = d1 + (f1 - f2);
-
-	d1 = d1 + d2;
-	d1 = d1 - d2;
-	d1 = d1 * d2;
-	d1 = d1 / d2;
-//	d1 = d1 % d2;
-
-    d1 = sqrt(d1);
-
-	d1 = sin(d1);
-	d1 = cos(d1);
-
-	d1 = tan(d1);
-
-	return d1;
+	bData_40003432 = 0;
+		
+	Data_400034d0 = 0.05; //?
+	Data_400034d8 = 0.045; //?
+	
+	Data_40004128.Data_192 = 100.0; //?
+	Data_40004128.Data_200 = 60.0; //?
+	
+	func_27c4(&Data_4000329c, &Data_400032a0);
+	
+	Data_40004128.Data_192 = Data_4000329c;
+	Data_40004128.Data_200 = Data_400032a0;
+	
+	//TODO
 }
 
 
